ab8b1c8f182069b9a6b15e058e3b6572e8cf51ce
Ryan Divigalpitiya <rdivigal@uwo.ca>
2021-03-29 10:47:41 -0400

    Dataset added. Initial python file added

---
+++ CNN_model_eval.py
@@ -0,0 +1,24 @@
+# CS 9542 Final Project
+# Responsibilities: Loading data + presentation deck (Heather), Building NN (Ryan), Evaluation Metrics (Mo)
+# March 29 – Set up Git Hub repository
+# March 31 – Loading data phase is complete (converted to tensor flow)
+# April 2 – NN base model complete, Evaluation metrics skeleton code complete
+# April 3 & April 4 – create presentation deck
+# April 4 – Practice run through of presentation (9 minutes presentation, 3 minutes Q & A)
+# ---------------------------------------------------------------------------------------------------------
+## Phase 1: ETL (Data Loading) // Heather -----------------------------------------------------------------
+#// code goes here
+
+
+
+## Phase 2: Prelim Model Buidling // Ryan -----------------------------------------------------------------
+#// code goes here
+
+
+
+## Phase 3: Performance Evaluation Metrics // Mohmmd ------------------------------------------------------
+#// code goes here
+
+
+
+# EndOfFile------------------------------------------------------------------------------------------------
